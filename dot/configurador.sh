e7633d8f0619c7b4ee943100dc4e4d313541787c
Giuliana Marquesi <giuliana@riseup.net>
2018-01-13 17:37:55 -0200

    adicionado o PS1

--- dot/configurador.sh
+++ dot/configurador.sh
@@ -38,3 +38,10 @@ echo "--------------------------------------------"
 
 echo '[[ $- != *i* ]] && return' >> ~/.bashrc
 echo '[[ -z "$TMUX" ]] && exec tmux' >> ~/.bashrc
+
+echo "--------------------------------------------"
+echo "	Configurando o bashrc para que modifique o PS1"
+echo "--------------------------------------------"
+
+echo 'PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"' >> ~/.bashrc
+
